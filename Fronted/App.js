518e55b150ed4204d71c525350035ceafb1ffee2
Manishkumar Pandey <43494119+MANISH762000@users.noreply.github.com>
2024-08-09 18:44:53 +0530

    Create App.js

---
+++ Fronted/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import UserManagement from "./components/UserManagement";
+
+function App() {
+  return (
+    <div className="App">
+      <UserManagement />
+    </div>
+  );
+}
+
+export default App;
